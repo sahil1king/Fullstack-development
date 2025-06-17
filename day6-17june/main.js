2404fca8a5cf76e21ac86a96e63d7eafacbd4208
Sahil Bhardwaj <sahilbhardwaj4488@gmail.com>
2025-06-17 12:43:45 +0530

    javascript going on

--- day6-17june/main.js
+++ day6-17june/main.js
@@ -1,7 +1,29 @@
 console.log("Print hello!")
 
-var a=5;
+// let a=5;
+// {
+//     console.log(a); // will give error because consoled before declaring in bloack{}
+//     let a=20; 
+//     console.log(a);
+// }
+// console.log(a);
 
-let b='hello';
+var b=5;
+{
+    console.log(b); 
+    var b=20;       // will change the global value because block doesn't work on var
+    console.log(b);
+}
+console.log(b);
 
-const c=true;
+
+//can console the variable before decaring it 
+console.log(temp);  //will give undefined 
+var temp = "this is temp";
+console.log(temp)
+
+
+//can not console the variable before declaring it 
+//console.log(temp2);    // dead temporal zone (unless it is assigned a value)
+let temp2 = "this is temp 2";
+console.log(temp2);
---
+++ day6-17june/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let a=5;
+// {
+//     console.log(a); // will give error because consoled before declaring in bloack{}
+//     let a=20; 
+//     console.log(a);
+// }
+// console.log(a);
