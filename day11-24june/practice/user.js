d8fa2b04721442682e984486f5d93fbf58d2c9b5
Sahil Bhardwaj <sahilbhardwaj4488@gmail.com>
2025-06-24 15:14:14 +0530

    practice done

--- day11-24june/practice/user.js
+++ day11-24june/practice/user.js
@@ -1,15 +1,25 @@
-document.getElementById('fetchUser').addEventListener('click', function() {
-            axios.get('https://randomuser.me/api/')
-                .then(function(response) {
-                    const user = response.data.results[0];
-                    const userInfo = `
-                        <p>Name: ${user.name.first} ${user.name.last}</p>
-                        <p>Email: ${user.email}</p>
-                        <img src="${user.picture.large}" alt="User Picture">
-                    `;
-                    document.getElementById('userInfo').innerHTML = userInfo;
-                })
-                .catch(function(error) {
-                    console.error('Error fetching user:', error);
-                });
+document.getElementById('newuser').addEventListener('click', function() {
+    axios.get('https://randomuser.me/api/')
+        .then(function(response) {
+            const user = response.data.results[0];
+            const userInfo = `
+                <img src="${user.picture.large}" alt="User Picture">
+                <p>Name: ${user.name.first} ${user.name.last}</p>
+                <p>Email: ${user.email}</p>
+            `;
+            document.getElementById('find').innerHTML = userInfo;
+            
+            localStorage.setItem('userInfo', userInfo);
+        })
+        .catch(function(error) {
+            console.error('Error fetching user:', error);
         });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const savedUserInfo = localStorage.getItem('userInfo');
+    if (savedUserInfo) {
+        document.getElementById('find').innerHTML = savedUserInfo;
+    }
+});
